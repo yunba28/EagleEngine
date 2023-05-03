c372e40c47a7c1ead4266c662f9efe3025daeb1b
yunba28 <70497005+yunba28@users.noreply.github.com>
2023-05-03 14:26:09 +0900

    ObjectPtrの作成開始

---
+++ EagleEngine/Engine/Private/Core/ObjectPtr.cpp
@@ -0,0 +1,23 @@
+﻿#include <Core/ObjectPtr.hpp>
+
+#include <CoreTypes.hpp>
+#include <Container/HashSet.hpp>
+
+namespace eagle
+{
+	HashSet<uintptr> gObjectTable;
+
+	bool RegisterObjectHandle(Object* inObject)
+	{
+		
+		return false;
+	}
+	bool UnregisterObjectHandle(Object* inObject)
+	{
+		return false;
+	}
+	bool IsValidByObject(Object* inObject)
+	{
+		return false;
+	}
+}
--- EagleEngine/Engine/Public/Core/ObjectPtr.hpp
+++ EagleEngine/Engine/Public/Core/ObjectPtr.hpp
@@ -1,10 +1,20 @@
 ﻿#pragma once
 
-#include <CoreFwd.hpp>
+#include <Concept/IsType.hpp>
 
 namespace eagle
 {
+	namespace Concept
+	{
+		template<class ObjectA, class ObjectB>
+		concept ConvertibleObject =
+			(Concept::IsObject<ObjectA> && Concept::IsObject<ObjectB>) ||
+			std::is_base_of_v<ObjectA,ObjectB> || std::is_base_of_v<ObjectB, ObjectA>;
+	}
 
+	bool RegisterObjectHandle(Object* inObject);
+	bool UnregisterObjectHandle(Object* inObject);
+	bool IsValidByObject(Object* inObject);
 
 	template<class ObjectType>
 	class ObjectPtr final
